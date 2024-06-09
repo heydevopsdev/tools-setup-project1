d9032a32d3a07b584ac17f29fae3609af74abac0
heydevopsdev <heydevopsdev@gmail.com>
2024-06-09 13:24:29 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -7,6 +7,8 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
+
+sudo apt-get update -y 
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
